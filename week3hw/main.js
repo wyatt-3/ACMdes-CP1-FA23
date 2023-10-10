8a49ce08bc1cd483351b029a8dd3793719866555
wyatt-3 <wyatt@dolphin3.xyz>
2023-10-10 17:04:41 +0200

    updates

--- week3hw/main.js
+++ week3hw/main.js
@@ -2,3 +2,4 @@ function changeImage (fileName) {
     let img = document.querySelector('#specialimage');
     img.setAttribute("src", fileName);
 }
+let myInterval = window.setInterval(switchImage, 2000);
---
+++ week4/inclass/main.js
@@ -0,0 +1,13 @@
+
+
+let switchImage = function(event){
+    console.log(event.target.id)
+    document.getElementById("largeImg").src=event.target.src
+}
+
+
+
+document.getElementById("b1").addEventListener("click", switchimage)
+document.getElementById("b1").addEventListener("click", switchimage)
+document.getElementById("b1").addEventListener("click", switchimage)
+document.getElementById("b1").addEventListener("click", switchimage)
